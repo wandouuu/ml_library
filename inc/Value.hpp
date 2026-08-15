c812031dbc22aeae6fb7ad75cc43d822243fdcde
wandouuu <130308899+wandouuu@users.noreply.github.com>
2026-08-15 18:04:55 -0400

    chore: set up basic project structure

---
+++ inc/Value.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef VALUE_H
+#define VALUE_H
+
+
+
+#endif
---
+++ src/Value.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <memory>
+#include <functional>
+#include <cmath>
+#include <iostream>
+
+class Value: std::
