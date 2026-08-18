514a3419b3bd5f16e49cf0fd9d91cb303aff2ec8
wandouuu <130308899+wandouuu@users.noreply.github.com>
2026-08-17 21:17:49 -0400

    feat: add value class template for autograd engine

--- inc/Value.hpp
+++ inc/Value.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
-#ifndef VALUE_H
-#define VALUE_H
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
 
+class Value : public std::enable_shared_from_this<Value> {
+    private:
+        double data; // holding the value of tensor
+        double grad; // gradient
 
+        std::vector<std::shared_ptr<void()>> prev; // vector of pointers towards parent nodes
 
-#endif
+        std::string op; // operand
+        std::function<void()> backward; // backward function
+    
+    public:
+        Value();
+        ~Value();
+};
