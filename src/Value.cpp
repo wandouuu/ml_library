1f3757c8cb6df7457ecd5e7e9d9bd914164c3494
wandouuu <130308899+wandouuu@users.noreply.github.com>
2026-08-28 16:21:56 -0400

    feat: add _backward function for the addition operations

--- src/Value.cpp
+++ src/Value.cpp
@@ -36,7 +36,14 @@ std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& lhs, const std::s
     prev.push_back(lhs);
     prev.push_back(rhs);
 
-    return std::make_shared<Value>(lhs->data + rhs->data, 0.0, prev, "+");
+    std::shared_ptr<Value> out = std::make_shared<Value>(lhs->data + rhs->data, 0.0, prev, "+");
+
+    out->_backward = [lhs, rhs, out](){
+        lhs->grad = out->data * 1;
+        rhs->grad = out->data * 1;
+    };
+
+    return out;
     
 }
 
@@ -52,7 +59,14 @@ std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& lhs, double rhs){
     prev.push_back(lhs);
     prev.push_back(rhs_ptr);
 
-    return std::make_shared<Value>(lhs->data + rhs, 0.0, prev, "+");
+    std::shared_ptr<Value> out = std::make_shared<Value>(lhs->data + rhs, 0.0, prev, "+");
+
+    out->_backward = [lhs, rhs_ptr, out](){
+        lhs->grad = out->data * 1;
+        rhs_ptr->grad = out->data * 1;
+    };  
+
+    return out;
 
 }
 
