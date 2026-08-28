29db3a59c8b387bc6a2cd7a6b02d0aa79bb92142
wandouuu <130308899+wandouuu@users.noreply.github.com>
2026-08-27 20:51:05 -0400

    feat: add addition operators on Value objects

--- src/Value.cpp
+++ src/Value.cpp
@@ -39,3 +39,23 @@ std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& lhs, const std::s
     return std::make_shared<Value>(lhs->data + rhs->data, 0.0, prev, "+");
     
 }
+
+std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& lhs, double rhs){
+    
+    if(!lhs){
+        return nullptr;
+    }
+
+    std::shared_ptr<Value> rhs_ptr = std::make_shared<Value>(rhs, 0.0, std::vector<std::shared_ptr<Value>>{}, "");
+
+    std::vector<std::shared_ptr<Value>> prev;
+    prev.push_back(lhs);
+    prev.push_back(rhs_ptr);
+
+    return std::make_shared<Value>(lhs->data + rhs, 0.0, prev, "+");
+
+}
+
+std::shared_ptr<Value> operator+(double lhs, const std::shared_ptr<Value>& rhs){
+    return rhs + lhs;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 
 int main(){
-    Value a = Value(5);
-    std::cout << a.get_data();
-    std::cout << a.get_grad();
-    
+    std::shared_ptr<Value> a = std::make_shared<Value>(5);
+    std::shared_ptr<Value> b = std::make_shared<Value>(3);
+
+    std::shared_ptr<Value> c = a + b;
+
+    std::cout << c->get_data() << std::endl;
+
     return 0;
 }
